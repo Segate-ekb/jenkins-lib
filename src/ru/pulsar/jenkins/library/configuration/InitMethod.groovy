c57df82509edc28dcef7f592c5bfa45e4e2733bc
ivanovEV <ivanovev@melonfashion.com>
2024-01-18 15:33:37 +0300

    Фикс

--- src/ru/pulsar/jenkins/library/configuration/InitMethod.groovy
+++ src/ru/pulsar/jenkins/library/configuration/InitMethod.groovy
@@ -2,7 +2,7 @@ package ru.pulsar.jenkins.library.configuration
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-enum initMethod {
+enum InitMethod {
     @JsonProperty("fromSource")
     SOURCE,
 
